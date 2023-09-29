45906345785302d86c9c2a3676e5d331c0ab9338
gohan5858 <kuromedaka1003@gmail.com>
2023-09-29 17:01:51 +0900

    [Change] Healthのアクセス方法を変更

--- Assets/Scripts/3D/models/PlayerModel.cs
+++ Assets/Scripts/3D/models/PlayerModel.cs
@@ -2,10 +2,11 @@ public static class PlayerModel
 {
     public static float MoveSpeed { get; set; }
     public static int AttackPower { get; set; }
-    public static int Health { get; set; }
+    public static int Health { get { return health; } }
+    private static int health = 100;
 
     public static int TakeDamage(int damage)
     {
-        return Health -= damage;
+        return health -= damage;
     }
 }
